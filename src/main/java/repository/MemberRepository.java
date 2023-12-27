7b265809498cff346b650523be8002e40d253ed6
JHyun0302 <wogustkfkd11@gmail.com>
2023-12-27 16:13:41 +0900

    :sparkles: [Feat] repository

--- src/main/java/repository/MemberRepository.java
+++ src/main/java/repository/MemberRepository.java
@@ -1,32 +1,66 @@
 package main.java.repository;
 
 import java.util.List;
-import main.java.dto.SignUpRequestDto;
-import main.java.exception.NoResourceException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import main.java.model.DiscountInfo;
 import main.java.model.Member;
 import main.java.utils.ExcelReader;
-import main.java.utils.ExcelWriter;
 
 public class MemberRepository {
     private static final MemberRepository memberRepository = new MemberRepository();
-    private List<Member> members = getMembers();
+    private List<Member> members = ExcelReader.readExcel();
 
     public static MemberRepository getInstance() {
         return memberRepository;
     }
 
-    private static List<Member> getMembers() {
-        return ExcelReader.readExcel();
+    //    public void save() {
+//        ExcelWriter.writeExcel();
+//    }
+
+    public List<Member> getMembers() {
+//        checkMembers();
+        return members;
     }
 
-    public void save(SignUpRequestDto signUpRequestDto) {
-        ExcelWriter.writeExcel(signUpRequestDto);
+    // 엑셀 입력 테스트
+    private void checkMembers() {
+        List<Map<DiscountInfo, String>> collect = members.stream()
+                .map(member -> member.getDiscounts())
+                .collect(Collectors.toList());
+
+        List<String> collect1 = members.stream()
+                .map(member -> member.getName())
+                .collect(Collectors.toList());
+
+        for (String name : collect1) {
+            System.out.println("name = " + name);
+        }
+
+        List<Integer> collect3 = members.stream()
+                .map(member -> member.getFee())
+                .collect(Collectors.toList());
+
+        for (Integer fee : collect3) {
+            System.out.println("fee = " + fee);
+        }
+
+        List<String> collect2 = members.stream()
+                .map(member -> member.getMembership())
+                .collect(Collectors.toList());
+
+        for (String membership : collect2) {
+            System.out.println("memberShip = " + membership);
+        }
+
+        for (Map<DiscountInfo, String> discountInfoStringMap : collect) {
+            for (Entry<DiscountInfo, String> discountInfoStringEntry : discountInfoStringMap.entrySet()) {
+                System.out.println("discountInfoStringEntry.getKey() = " + discountInfoStringEntry.getKey());
+                System.out.println("discountInfoStringEntry.getValue() = " + discountInfoStringEntry.getValue());
+            }
+        }
     }
 
-    public Member findByLoginId(String loginId) {
-        return members.stream()
-                .filter(member -> member.getLoginId().equals(loginId))
-                .findFirst()
-                .orElseThrow(() -> NoResourceException.errorMessage());
-    }
 }
