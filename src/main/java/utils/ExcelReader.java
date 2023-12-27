6b88def1d03d29b9b90e69c769a0183d982b6701
JHyun0302 <wogustkfkd11@gmail.com>
2023-12-27 16:11:49 +0900

    :sparkles: [Feat] Console

--- src/main/java/utils/ExcelReader.java
+++ src/main/java/utils/ExcelReader.java
@@ -2,14 +2,17 @@ package main.java.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import main.java.model.AgeInfo;
 import main.java.model.DiscountInfo;
 import main.java.model.Member;
-import main.java.model.MemberShip;
+import main.java.model.Membership;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -31,14 +34,23 @@ public class ExcelReader {
                 Map<DiscountInfo, String> userDiscounts = new HashMap<>();
                 Row row = rowIterator.next();
                 String name = dataFormatter.formatCellValue(row.getCell(0));
-                String loginId = dataFormatter.formatCellValue(row.getCell(1));
-                String password = dataFormatter.formatCellValue(row.getCell(2));
-                int age = Integer.parseInt(dataFormatter.formatCellValue(row.getCell(3)));
-                String membership = dataFormatter.formatCellValue(row.getCell(4));
+                int age = Integer.parseInt(dataFormatter.formatCellValue(row.getCell(1)));
+                LocalDate createPayDate = LocalDate.parse(dataFormatter.formatCellValue(row.getCell(2)),
+                        DateTimeFormatter.ofPattern("MM/d/yy"));
+                int cumulativeAmount = Integer.parseInt(dataFormatter.formatCellValue(row.getCell(3)));
+
+                /**
+                 * DB로 바꾼다면...
+                 * TODO cumulativeAmount, create_paid_date 묶어서 결제 관련 테이블(객체)로 만들기
+                 */
+
+                int feeByAge = AgeInfo.getFeeByAge(age);
+
+                String membership = Membership.getMembership(cumulativeAmount);
 
                 Map<DiscountInfo, String> discounts = new HashMap<>();
 
-                for (int i = 5; i < row.getLastCellNum(); i++) {
+                for (int i = 4; i < row.getLastCellNum(); i++) {
                     String cellValue = dataFormatter.formatCellValue(row.getCell(i));
                     if ("o".equals(cellValue) || "x".equals(cellValue)) {
                         String discountInfoName = dataFormatter.formatCellValue(sheet.getRow(0).getCell(i));
@@ -47,10 +59,8 @@ public class ExcelReader {
                     }
                 }
 
-                MemberShip memberShip = MemberShip.getInstance(membership);
-
                 // SignUpRequestDto를 생성할 때 멤버십 정보도 전달합니다.
-                members.add(Member.createMember(name, loginId, password, age, discounts, memberShip));
+                members.add(Member.createMember(name, feeByAge, discounts, membership, createPayDate));
             }
         } catch (IOException e) {
             throw new IllegalArgumentException(e);
