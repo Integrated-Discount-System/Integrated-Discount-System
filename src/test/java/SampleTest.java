8119020e215146b3414c8eca3586f5496d9a7715
amanecopse <amanecopse@gmail.com>
2023-12-27 17:54:07 +0900

    [Test] Add sample test code

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+    @Test
+    public void 샘플테스트(){
+        Assertions.assertEquals(2, 1+1);
+    }
+}
